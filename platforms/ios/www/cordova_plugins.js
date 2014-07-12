670ed867757fb95c9acdc641a20c5bc973fb3072
Henry Nguyen <henry@dxconcept.com>
2014-07-12 16:08:43 -0700

    Added iOS build. Updated icons for iOS and Android. Uppercased name.

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/me.apla.cordova.app-preferences/www/apppreferences.js",
+        "id": "me.apla.cordova.app-preferences.apppreferences",
+        "clobbers": [
+            "plugins.appPreferences"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+]
+});
